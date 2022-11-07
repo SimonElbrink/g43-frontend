a23f1a97cbfadc000f996f4c5a96e56faf359037
Simon Elbrink <simon.elbrink@lexicon.se>
2022-11-07 12:19:41 +0100

    React - Fundamentals - Class Component

--- 3-react/react-fundamentals/src/index.js
+++ 3-react/react-fundamentals/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './ex1-class-components/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // const welcomeMessage = react.createElement("h1", null, "Hello Group 43!!");
 
-const messageJSX = <h1>Hello Group 43!!</h1>;
+//const messageJSX = <h1>Hello Group 43!!</h1>;
 
 
-root.render(messageJSX);
+root.render(<App/>);
