c04891a74b5a07313f655a2777fc5abeba1eca6a
Simon Elbrink <simon.elbrink@lexicon.se>
2022-11-21 08:29:13 +0100

    Part 3 - Create form added

--- src/CrudDemo.js
+++ src/CrudDemo.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Form from './Form';
 import Table from './Table';
 
 const CrudDemo = () => {
@@ -51,10 +52,37 @@ const CrudDemo = () => {
         })
     }
 
+    const savePerson = async (data) => {
+        await axios.post(API_URL, data).then(
+            (response) => {
+
+                console.log(response);
+
+                if (response.status === 201) {
+                    console.log(`API: (Save) Request was executed Successfully!`);
+                    setLoadData(!loadData)
+
+                } else if (response.status === 400){
+                    
+
+                }else {
+                    console.error(`API: Request was executed with status code ${response.status}`);
+                }
+
+            }
+        ).catch((error) => {
+            console.warn(`API: Request Encounter an Error ${error}`);
+        })
+    }
+
 
     return (
         <div>
+            <Form handleSave={savePerson}/>
+
             <Table data={persons} handleDelete={deletePersonByID}/>
+
+            
 
         </div>
     );
